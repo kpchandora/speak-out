ca38448b6bb86a13146b25a87edc556dcceec768
Kalpesh Chandora <kalpeshchandora12@gmail.com>
2020-08-05 23:16:44 +0530

    Remove unused code

--- app/src/main/java/com/speakout/utils/FirebaseUtils.kt
+++ app/src/main/java/com/speakout/utils/FirebaseUtils.kt
@@ -9,44 +9,6 @@ import com.google.firebase.storage.FirebaseStorage
 
 object FirebaseUtils {
 
-    fun getFirebaseFunction(functionName: String) =
-        FirebaseFunctions.getInstance().getHttpsCallable(functionName)
-
-    object FirestoreUtils {
-
-        fun getRef() = FirebaseFirestore.getInstance()
-
-        fun getUsersRef() =
-            getRef().collection(NameUtils.DatabaseRefs.userDetailsRef)
-
-        fun getSingleUserRef(userId: String) = getRef().document("user_details/$userId")
-
-        fun getUsersPostRef(userId: String, postId: String) =
-            getRef().document("user_details/$userId/posts/$postId")
-
-        fun getTagsRef() = getRef().collection(NameUtils.DatabaseRefs.tags)
-
-        fun getAllPostsRef() = getRef().collection(NameUtils.DatabaseRefs.postsRef)
-
-        fun getSinglePostRef(postId: String) = getAllPostsRef().document(postId)
-
-        fun getPostSingleLikeRef(postId: String, userId: String) =
-            getRef().document("post_likes/$postId/users/$userId")
-
-        fun getPostLikesRef(postId: String) = getRef().document("post_likes/$postId")
-
-        fun getFollowersFollowingsRef(userId: String) =
-            getRef().document("followers_followings_count/${userId}")
-
-        fun getFollowingsRef(userId: String) =
-            getRef().document("/followings/${userId}")
-
-        fun isFollowingRef(userId: String, selfId: String) =
-            getRef().document("/followings/${selfId}/users/${userId}")
-
-
-    }
-
     fun currentUser() = FirebaseAuth.getInstance().currentUser
 
     fun userId() = currentUser()?.uid
@@ -59,15 +21,5 @@ object FirebaseUtils {
     fun getPostsStorageRef() = FirebaseStorage.getInstance().reference.child("posts")
 
     fun getProfilePictureStorageRef() = FirebaseStorage.getInstance().reference.child("profile")
-
-    fun getReference() = FirebaseDatabase.getInstance().reference
-
-    fun getTagsRef() = getReference().child("tags")
-
-    enum class Data {
-        PRESET,
-        ABSENT,
-        CANCELLED
-    }
 
 }

267aa47f93ad9fde5142c27d39b6d0b55db42b00
Kalpesh Chandora <kalpeshchandora12@gmail.com>
2020-08-19 23:46:43 +0530

    Add user image to notification

--- app/src/main/java/com/speakout/fcm/SpeakOutFirebaseMessagingService.kt
+++ app/src/main/java/com/speakout/fcm/SpeakOutFirebaseMessagingService.kt
@@ -2,11 +2,11 @@ package com.speakout.fcm
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
-import android.graphics.BitmapFactory
+import android.graphics.*
 import android.os.Build
 import android.os.Bundle
-import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.google.firebase.messaging.FirebaseMessagingService
@@ -22,6 +22,7 @@ import timber.log.Timber
 import java.io.IOException
 import java.net.URL
 
+
 /**
  * Created by Kalpesh on 09/08/20.
  */
@@ -35,10 +36,17 @@ public class SpeakOutFirebaseMessagingService : FirebaseMessagingService() {
         const val FOLLOW = "follow"
         const val UNFOLLOW = "unfollow"
         const val LIKE = "like"
-        const val REMOVE_LIKE = "removeLike"
         const val NOTIFICATION_ID = "notificationId"
         const val USER_ID = "userId"
-        const val POST_IMAGE_URL = "postImageUrl"
+        const val OTHER = "other"
+        const val USER_IMAGE_URL = "userImageUrl"
+    }
+
+    override fun onNewToken(token: String) {
+        Timber.d("Token: $token")
+        GlobalScope.launch {
+            userRepository.updateFcmToken(token)
+        }
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
@@ -56,6 +64,9 @@ public class SpeakOutFirebaseMessagingService : FirebaseMessagingService() {
                     LIKE -> {
                         sendLikeNotification(it)
                     }
+                    OTHER -> {
+                        //This is for other notifications such as some events
+                    }
                 }
             }
         }
@@ -63,84 +74,35 @@ public class SpeakOutFirebaseMessagingService : FirebaseMessagingService() {
 
     private fun sendFollowNotification(map: MutableMap<String, String>) {
         val bundle = Bundle()
-        val notificationLayout = RemoteViews(packageName, R.layout.layout_custom_notification)
-        notificationLayout.setTextViewText(
-            R.id.tv_notification_desc,
-            "${map["username"]} started following you."
-        )
 
         val channelId = "new_followers"
-        val customNotification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher)
-//            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
-//            .setContentTitle("SpeakOut")
-            .setStyle(NotificationCompat.BigTextStyle())
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentTitle("${map["username"]} started following you.")
-//            .setCustomContentView(notificationLayout)
-            .setAutoCancel(true)
-            .build()
+        val notification = getNotificationBuilder(channelId = channelId)
+        notification.setContentTitle("${map["username"]} started following you.")
 
-        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                "New Followers",
-                NotificationManager.IMPORTANCE_DEFAULT
-            )
-            manager.createNotificationChannel(channel)
+        getUserImageBitmap(map)?.let {
+            notification.setLargeIcon(it)
         }
 
-        manager.notify("${map[NOTIFICATION_ID]}", 0, customNotification)
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        createChannel(channelId, "New Followers", manager)
+        manager.notify("${map[NOTIFICATION_ID]}", 0, notification.build())
 
         Timber.d("Notification sent")
     }
 
     private fun sendLikeNotification(map: MutableMap<String, String>) {
-        val notificationLayout = RemoteViews(packageName, R.layout.layout_custom_notification)
-        notificationLayout.setTextViewText(
-            R.id.tv_notification_desc,
-            "${map["username"]}(${map["name"]}) liked your post."
-        )
 
         val channelId = "likes"
-        val customNotification = NotificationCompat.Builder(this, channelId)
-            .setSmallIcon(R.mipmap.ic_launcher)
-//            .setStyle(NotificationCompat.DecoratedCustomViewStyle())
-//            .setContentTitle("SpeakOut")
-            .setStyle(NotificationCompat.BigTextStyle())
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setContentText("${map["username"]}(${map["name"]}) liked your post.")
-//            .setCustomContentView(notificationLayout)
-            .setAutoCancel(true)
-            .setColor(ContextCompat.getColor(this, R.color.indigo_500))
+        val notification = getNotificationBuilder(channelId = channelId)
+        notification.setContentText("${map["username"]}(${map["name"]}) liked your post.")
 
-        try {
-            map[POST_IMAGE_URL]?.let {
-                val bitmap = BitmapFactory.decodeStream(URL(it).openConnection().getInputStream())
-                if (bitmap != null) {
-                    customNotification.setLargeIcon(bitmap)
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
+        getUserImageBitmap(map)?.let {
+            notification.setLargeIcon(it)
         }
 
         val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                "Likes",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            channel.setShowBadge(true)
-            channel.importance = NotificationManager.IMPORTANCE_HIGH
-            manager.createNotificationChannel(channel)
-        }
-
-        manager.notify("${map[NOTIFICATION_ID]}", 0, customNotification.build())
+        createChannel(channelId, "Likes", manager)
+        manager.notify("${map[NOTIFICATION_ID]}", 0, notification.build())
 
         Timber.d("Notification sent")
     }
@@ -150,10 +112,85 @@ public class SpeakOutFirebaseMessagingService : FirebaseMessagingService() {
         manager.cancel("${map[NOTIFICATION_ID]}", 0)
     }
 
-    override fun onNewToken(token: String) {
-        Timber.d("Token: $token")
-        GlobalScope.launch {
-            userRepository.updateFcmToken(token)
+    private fun getUserImageBitmap(map: MutableMap<String, String>): Bitmap? {
+        try {
+            map[USER_IMAGE_URL]?.let {
+                val bitmap = BitmapFactory.decodeStream(URL(it).openConnection().getInputStream())
+                return getCircleBitmap(bitmap)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return null
+    }
+
+    private fun getNotificationBuilder(
+        pendingIntent: PendingIntent? = null,
+        channelId: String
+    ): NotificationCompat.Builder {
+        return NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setStyle(NotificationCompat.BigTextStyle())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+            .setColor(ContextCompat.getColor(this, R.color.indigo_500))
+
+    }
+
+    private fun createChannel(
+        channelId: String,
+        channelName: String,
+        manager: NotificationManager
+    ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+            channel.setShowBadge(true)
+            channel.importance = NotificationManager.IMPORTANCE_HIGH
+            manager.createNotificationChannel(channel)
         }
     }
+
+    private fun getCircleBitmap(bitmap: Bitmap): Bitmap {
+        val output: Bitmap
+        val srcRect: Rect
+        val dstRect: Rect
+        val r: Float
+        val width: Int = bitmap.width
+        val height: Int = bitmap.height
+
+        if (width > height) {
+            output = Bitmap.createBitmap(height, height, Bitmap.Config.ARGB_8888)
+            val left = (width - height) / 2
+            val right = left + height
+            srcRect = Rect(left, 0, right, height)
+            dstRect = Rect(0, 0, height, height)
+            r = height / 2.toFloat()
+        } else {
+            output = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888)
+            val top = (height - width) / 2
+            val bottom = top + width
+            srcRect = Rect(0, top, width, bottom)
+            dstRect = Rect(0, 0, width, width)
+            r = width / 2.toFloat()
+        }
+
+        val canvas = Canvas(output)
+
+        val color = -0xbdbdbe
+        val paint = Paint()
+
+        paint.isAntiAlias = true
+        canvas.drawARGB(0, 0, 0, 0)
+        paint.color = color
+        canvas.drawCircle(r, r, r, paint)
+        paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
+        canvas.drawBitmap(bitmap, srcRect, dstRect, paint)
+        bitmap.recycle();
+        return output
+    }
+
 }

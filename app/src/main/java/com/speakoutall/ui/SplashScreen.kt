ec63b19e86d4204721316299fbd70facae9a328a
Kalpesh Chandora <kalpeshchandora12@gmail.com>
2023-04-02 00:04:09 +0530

    Update deps (#30)
    * Update dependency versions
    
    * Update dependencies to latest versions

--- app/src/main/java/com/speakoutall/ui/SplashScreen.kt
+++ app/src/main/java/com/speakoutall/ui/SplashScreen.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatDelegate
 
 class SplashScreen : AppCompatActivity() {
@@ -13,7 +14,7 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
-        handler = Handler()
+        handler = Handler(Looper.getMainLooper())
         handler?.postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
             finish()
